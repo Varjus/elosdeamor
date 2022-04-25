e7b71e148bde1ae6a1f1c4e692a3ae09e68ba55a
Clayton Varjus <clayton.varjus@gvdobrasil.local>
2022-04-25 17:07:33 -0300

    Atualização Procfile

--- elosdeamor/settings.py
+++ elosdeamor/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
-import django_on_heroku
+import django_on_herokupip 
 import dj_database_url
 
 on_heroku = os.getenv('ON_HEROKU', 'False')
